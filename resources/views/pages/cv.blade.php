0734d78dc27ca17c2bb24e3a655bf60b9185ee45
Jeffrey Siebach <jsiebach@gmail.com>
2019-02-11 17:07:01 -0600

    new CV

--- resources/views/pages/cv.blade.php
+++ resources/views/pages/cv.blade.php
@@ -6,7 +6,7 @@
 
 @section('body')
     <div class="resume">
-        <a class="btn btn-success pull-right" href="/uploads/Siebach_CV_2-28-2018.pdf" target="_blank"><i class="fa fa-file-pdf-o"></i> Download PDF</a>
+        <a class="btn btn-success pull-right" href="/uploads/Siebach_CV_1-1-2019.pdf" target="_blank"><i class="fa fa-file-pdf-o"></i> Download PDF</a>
         @include('partials.page-title')
         <h2>Education</h2>
         <ul>
