5521a922ff494e124645f5ff1b5953675eb36f17
Jeff Siebach <jsiebach@gmail.com>
2021-03-24 18:56:18 -0500

    new cv

--- app/Http/Requests/SlideshowRequest.php
+++ app/Http/Requests/SlideshowRequest.php
@@ -3,8 +3,9 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
 
-class SlideshowRequest extends \Backpack\CRUD\app\Http\Requests\CrudRequest
+class SlideshowRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
