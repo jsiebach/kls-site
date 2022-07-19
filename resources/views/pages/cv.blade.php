2b8b7f54dfbab4f66ec73390c651718552415c97
Jeff Siebach <jsiebach@gmail.com>
2022-07-19 08:14:48 -0500

    CV

--- resources/views/pages/cv.blade.php
+++ resources/views/pages/cv.blade.php
@@ -8,6 +8,6 @@
     <div class="resume" style="height: 100%">
         <a class="btn btn-success pull-right" href="https://kirstensiebach.com/uploads/Siebach_CV_7-14-2022.pdf" target="_blank"><i class="fa fa-file-pdf-o"></i> Download PDF</a>
         @include('partials.page-title')
-        <embed src="https://kirstensiebach.com/uploads/Siebach_CV_9-21-2021.pdf" type="application/pdf" width="100%" height="100%">
+        <embed src="https://kirstensiebach.com/uploads/Siebach_CV_7-14-2022.pdf" type="application/pdf" width="100%" height="100%">
     </div>
 @endsection
